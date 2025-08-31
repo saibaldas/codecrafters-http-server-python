dd7d56daca0a6ef7e9e05f64df7799e5df55a07e
saibaldasprivate <saibal.das.plano@gmail.com>
2025-08-31 14:00:48 -0500

    Respond with 200 Step 2

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket = socket.create_server(("localhost", 4221))
+    server_socket.accept()[0].send(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
