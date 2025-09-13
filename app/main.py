939643e80b8f38cdab986023fa2fe46c56a46c53
saibaldas <saibal_das@hotmail.com>
2025-09-13 09:54:10 -0500

    Extract URL path -corrected

--- app/main.py
+++ app/main.py
@@ -8,18 +8,23 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221))
-    while True:
-        client_socket, _ = server_socket.accept()
-        print("Socket accepted a connection!")
-        data = client_socket.recv(1024)
-        request = data.decode()
-        print(request)
-        if request.startswith("GET / HTTP/1.1"):
-            response = "HTTP/1.1 200 OK\r\n\r\n"
-        else:
-            response = "HTTP/1.1 404 Not Found\r\n\r\n"
-        client_socket.sendall(response.encode())
-
+    try:
+        while True:
+            client_socket, _ = server_socket.accept()
+            print("Socket accepted a connection!")
+            data = client_socket.recv(1024)
+            request = data.decode()
+            print(request)
+            if request.startswith("GET / HTTP/1.1"):
+                response = "HTTP/1.1 200 OK\r\n\r\n"
+            else:
+                response = "HTTP/1.1 404 Not Found\r\n\r\n"
+            client_socket.sendall(response.encode())
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt received, exiting.")
+        pass
+    finally:
+        server_socket.close()   
 
 if __name__ == "__main__":
     main()
