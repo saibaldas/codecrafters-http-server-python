59f8a1c241631baec609fc70b98f8d42ad73f1f4
saibaldas <saibal_das@hotmail.com>
2025-09-13 09:32:13 -0500

    Extract URL path

--- app/main.py
+++ app/main.py
@@ -8,7 +8,17 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221))
-    server_socket.accept()[0].send(b"HTTP/1.1 200 OK\r\n\r\n")
+    while True:
+        client_socket, _ = server_socket.accept()
+        print("Socket accepted a connection!")
+        data = client_socket.recv(1024)
+        request = data.decode()
+        print(request)
+        if request.startswith("GET / HTTP/1.1"):
+            response = "HTTP/1.1 200 OK\r\n\r\n"
+        else:
+            response = "HTTP/1.1 404 Not Found\r\n\r\n"
+        client_socket.sendall(response.encode())
 
 
 if __name__ == "__main__":
